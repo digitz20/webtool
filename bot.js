024303699aae9b882316d4a4ba24f3463869f286
digitz20 <alaekekaebuka200@gmail.com>
2026-01-27 13:30:03 +0100

    hhe

--- bot.js
+++ bot.js
@@ -566,7 +566,13 @@ async function main(io) {
   try {
     browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--disable-gpu'
+      ],
     });
 
     while (true) {
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
 const path = require('path');
-const fs = require('fs');
-const { main } = require('./bot');
+const { main } = require('./bot.js');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const leadsFilePath = path.join(__dirname, 'leads.json');
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -19,37 +19,14 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-
-  if (fs.existsSync(leadsFilePath)) {
-    const leads = JSON.parse(fs.readFileSync(leadsFilePath));
-    socket.emit('initial-leads', leads);
-  }
-
-  socket.on('delete-industry', (industry) => {
-    if (fs.existsSync(leadsFilePath)) {
-        let leads = JSON.parse(fs.readFileSync(leadsFilePath));
-        const remainingLeads = leads.filter(l => l.industry !== industry);
-        fs.writeFileSync(leadsFilePath, JSON.stringify(remainingLeads, null, 2));
-        io.emit('initial-leads', remainingLeads);
-    }
-  });
-
-  socket.on('delete-leads', (leadsToDelete) => {
-    if (fs.existsSync(leadsFilePath)) {
-        let leads = JSON.parse(fs.readFileSync(leadsFilePath));
-        const remainingLeads = leads.filter(l => !leadsToDelete.includes(l.website));
-        fs.writeFileSync(leadsFilePath, JSON.stringify(remainingLeads, null, 2));
-        io.emit('initial-leads', remainingLeads);
-    }
-  });
-
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
-setTimeout(() => main(io), 0);
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
 });
+
+// Pass the io instance to the main bot function
+main(io);
